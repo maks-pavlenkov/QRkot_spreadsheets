a0e0a89bb4d497840273f479540115c1f44772c3
maks-pavlenkov <coolpavlenkov@mail.ru>
2023-10-03 00:39:39 +0300

    awd

--- app/core/base.py
+++
@@ -1,5 +0,0 @@
-"""Импорты класса Base и всех моделей для Alembic."""
-from app.models.charity_project import CharityProject  # noqa
-from app.models.donation import Donation  # noqa
-from app.models.user import User  # noqa
-from .db import Base  # noqa
